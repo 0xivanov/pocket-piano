81624afcc7175a001a003dc183646c91c686864a
Your Name <you@example.com>
2019-12-11 22:55:42 +0200

    another change

--- pocket_piano/lib/Screens/Home/Home.dart
+++ pocket_piano/lib/Screens/Home/Home.dart
@@ -25,6 +25,81 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Container(
+              width: 300.0,
+              padding: EdgeInsets.all(15.0),
+              child: FlatButton(
+                onPressed: () {},
+                child: Column(
+                  children: <Widget>[
+                    Image(image: AssetImage('assets/piano.png')),
+                    SizedBox(height: 5.0,),
+                    Text(
+                      "Keyboard",
+                      style: TextStyle(
+                        fontSize: 30.0
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              width: 235.0,
+              height: 300.0,
+              padding: EdgeInsets.symmetric(vertical: 22.0, horizontal: 15.0),
+              child: FlatButton(
+                onPressed: () {},
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 200.0,
+                      child: Image(image: AssetImage('assets/Note.png'))
+                    ),
+                    SizedBox(height: 5.0,),
+                    Text(
+                      "Learn to play",
+                      style: TextStyle(
+                        fontSize: 30.0
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              width: 220.0,
+              height: 220.0,
+              padding: EdgeInsets.all(0.0),
+              child: FlatButton(
+                onPressed: () {},
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 160.0,
+                      child: Image(image: AssetImage('assets/records.png'))
+                    ),
+                    SizedBox(height: 5.0,),
+                    Text(
+                      "Records",
+                      style: TextStyle(
+                        fontSize: 30.0
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
